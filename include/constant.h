f694949993a168964cad23705236a5093fa6d17d
Tryncha <juanes.aguirre26@gmail.com>
2026-06-10 14:18:06 -0500

    constants.h header renamed

--- include/constant.h
+++ include/constant.h
@@ -1,25 +1,23 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
+#ifndef CONSTANT_H
+#define CONSTANT_H
 
+#include "matrix.h"
 #include "tgaimage.h"
 
-namespace constants {
-constexpr int width{64};
-constexpr int height{64};
+namespace constant {
+constexpr int width{8192};
+constexpr int height{8192};
+}  // namespace constant
 
-constexpr double lower_limit{0.1};
-constexpr double upper_limit{0.9};
-
-namespace colors {
+namespace color {
 // clang-format off
-// attention: order         B    G    R    A
+// warning: order           B    G    R    A
 constexpr TGAColor white  {255, 255, 255, 255};
 constexpr TGAColor green  {  0, 255,   0, 255};
 constexpr TGAColor red    {  0,   0, 255, 255};
 constexpr TGAColor blue   {255, 128,  64, 255};
 constexpr TGAColor yellow {  0, 200, 255, 255};
 // clang-format on
-}  // namespace colors
-}  // namespace constants
+}  // namespace color
 
 #endif
